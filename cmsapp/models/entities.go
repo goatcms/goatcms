f195e3a3f6fda39061041c9e941a81e2be847c0e
Sebastian Pozoga <sebastian@pozoga.eu>
2017-03-17 00:55:27 +0100

    add !id as primary key type (support by pgsql and sqlite)

--- cmsapp/models/entities.go
+++ cmsapp/models/entities.go
@@ -1,14 +1,14 @@
 package models
 
 type User struct {
-	ID           int64  `json:"id" db:"id" sqltype:"!int!primary!auto" form:"?ID"`
+	ID           int64  `json:"id" db:"id" sqltype:"!id" form:"?ID"`
 	Email        string `json:"email" db:"Email" sqltype:"!char(400)" form:"Email"`
 	PasswordHash string `json:"passwordHash" db:"PasswordHash" sqltype:"!char(400)"`
 }
 
 // Article is a entity represent single article
 type Article struct {
-	ID      int64  `json:"id" db:"id" sqltype:"!int!primary!auto" form:"?ID"`
+	ID      int64  `json:"id" db:"id" sqltype:"!id" form:"?ID"`
 	Title   string `json:"title" db:"Title" sqltype:"!char(400)" form:"Title"`
 	Content string `json:"content" db:"Content" sqltype:"!char(400)" form:"Content"`
 	Image   string `json:"image" db:"Image" sqltype:"!char(400)"`
@@ -20,7 +20,7 @@ func ArticleFactory() interface{} {
 
 // Fragment storage single webpage fragment
 type Fragment struct {
-	ID    int64  `json:"id" db:"id" schema:"id" sqltype:"!int!primary!auto"`
+	ID    int64  `json:"id" db:"id" schema:"id" sqltype:"!id"`
 	Key   string `json:"key" db:"Key" schema:"Key" sqltype:"!char(400)"`
 	Value string `json:"value" db:"Value" schema:"Value" sqltype:"!char(400)"`
 }
