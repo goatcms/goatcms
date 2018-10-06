6d98857fd62532d62257eb8868f1f878c9cad3bd
Sebastian Pozoga <sebastian@pozoga.eu>
2018-10-07 01:59:07 +0200

    correct webslots

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,52 +1,11 @@
 #!/bin/bash
 set -e
 
-cat > config/config_dev.json << EndOfConfig
-{
-  "mailer": {
-    "smtp": {
-      "addr": "$SMTP_HOST",
-      "auth": {
-        "username": "$SMTP_USERNAME",
-        "password": "$SMTP_PASSWORD",
-        "identity": "$SMTP_IDENTITY"
-      }
-    }
-  },
-  "translate": {
-    "langs": "en, pl",
-    "default": "en"
-  },
-  "router": {
-    "host": ":80",
-    "static": {
-      "path": "./web/dist/",
-      "prefix": "/static/"
-    },
-    "security": {
-      "mode": "$SECURITY_MODE",
-      "cert": "/certs/fullchain.pem",
-      "key": "/certs/privkey.pem"
-    }
-  },
-  "database": {
-    "engine": "sqlite3",
-    "url": "file:main.db?cache=shared"
-  },
-  "app": {
-    "baseURL": "$APP_BASE_URL"
-  },
-  "oauth": {
-    "github": {
-      "app": "$OAUTH_GITHUB_APP",
-      "secret": "$OAUTH_GITHUB_SECRET"
-    }
-  }
-}
-EndOfConfig
-
-cp config/config_dev.json config/config_prod.json
-cp config/config_dev.json config/config_test.json
+if [ $MODE = "HTTPS" ]; then
+  sh -x /app/docker/https-config.sh
+else
+  sh -x /app/docker/http-config.sh
+fi
 
 ### Build database
 rm main.db ||:
